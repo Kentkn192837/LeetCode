ff788af760fc728caf95be6e7ddf89c5916257f8
Kentkn192837 <agriculture5012@gmail.com>
2021-06-19 14:09:05 +0900

    クイックソート

--- algorithm/quicksort.py
+++ algorithm/quicksort.py
@@ -2,11 +2,11 @@ def divide(p, a):
     if len(a) < 1:
         return ([], [])
     x, y = divide(p, a[1:])
-    a = a[0]
-    if a < p:
-        return ([a], x, y)
+    b = a[0]
+    if b < p:
+        return ([b], x, y)
     else:
-        return (x, [a] + y)
+        return (x, [b] + y)
 
 def quick_sort(a):
     if len(a) < 2:
