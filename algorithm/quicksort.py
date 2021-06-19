748494065f907d25ab07389091e871d022bf20fe
Kentkn192837 <agriculture5012@gmail.com>
2021-06-19 14:07:45 +0900

    クイックソート

--- algorithm/quicksort.py
+++ algorithm/quicksort.py
@@ -1,2 +1,16 @@
-if __name__ == '__main__':
-    pass
+def divide(p, a):
+    if len(a) < 1:
+        return ([], [])
+    x, y = divide(p, a[1:])
+    a = a[0]
+    if a < p:
+        return ([a], x, y)
+    else:
+        return (x, [a] + y)
+
+def quick_sort(a):
+    if len(a) < 2:
+        return a
+    p = a[0]
+    x, y = divide(p, a[1:])
+    return quick_sort(x) + [p] + quick_sort(y)
